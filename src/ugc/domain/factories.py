e515141f992854b4fdb18c17e9609434bcb2f21b
Roman Reznikov <romanreznikov2002@yandex.ru>
2022-06-23 16:12:21 +0400

    #8: refactor: refactor types and factories

--- src/ugc/domain/factories.py
+++ src/ugc/domain/factories.py
@@ -1,18 +1,20 @@
 from abc import ABC, abstractmethod
-from typing import Type
+from typing import Generic, Type, TypeVar
 
 from .types import BaseModel
 
+_BM = TypeVar("_BM", bound=BaseModel)
 
-class BaseModelFactory(ABC):
+
+class BaseModelFactory(ABC, Generic[_BM]):
     """Базовая фабрика моделей."""
 
-    cls: Type[BaseModel]
+    cls: Type[_BM]
 
     @abstractmethod
-    def create_new(self, **kwargs) -> BaseModel:
+    def create_new(self, **kwargs) -> _BM:
         """Создание нового объекта."""
 
-    def create_from_serialized(self, data: dict) -> BaseModel:
+    def create_from_serialized(self, data: dict) -> _BM:
         """Создание нового объекта из сериализованных данных."""
         return self.cls.from_dict(data)
